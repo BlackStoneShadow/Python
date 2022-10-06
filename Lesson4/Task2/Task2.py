e7d62dbcc102400ccf20e6fda0c55b6bc51292be
Tolik <ProtorskyAV@yandex.ru>
2022-10-06 23:07:05 +0300

    Modify Lesson2

--- Lesson4/Task2/Task2.py
+++ Lesson4/Task2/Task2.py
@@ -1,5 +1,6 @@
 #Задайте натуральное число N. 
 #Напишите программу, которая составит список простых множителей числа N.
+from math import sqrt
 
 def is_int(number):
     return number.isdigit()
@@ -11,6 +12,6 @@ while not is_int(str(number)):
 
 number = int(number)
 
-result = [(num, int(number / num)) for num in range(1, number) if not number % num]
+result = [(num, int(number / num)) for num in range(1, number + 1) if not number % num]
 
 print(result)
