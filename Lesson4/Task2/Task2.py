680a2b157352790b27e72f9f721245f2067d8daf
Tolik <ProtorskyAV@yandex.ru>
2022-10-08 23:15:27 +0300

    Correct Task2 add simple number list

--- Lesson4/Task2/Task2.py
+++ Lesson4/Task2/Task2.py
@@ -1,5 +1,6 @@
 #Задайте натуральное число N. 
 #Напишите программу, которая составит список простых множителей числа N.
+from math import sqrt
 
 def try_castinput(message, type):
     result = str()
@@ -11,8 +12,22 @@ def try_castinput(message, type):
 
     return result
 
+def is_simple(number, numbers):
+    for i in numbers:
+        if i > sqrt(number):
+            return True
+        elif not number % i:
+            return False
+    return True
+
 number = try_castinput("Input number:", int)
 
-result = [(num, number // num) for num in range(1, number + 1) if num % 2 and num % 3 and not number % num]
+simple_numbers = list()
+
+for num in range(2, number):
+    if is_simple(num, simple_numbers): 
+        simple_numbers.append(num)
+
+result = [(num, number // num) for num in simple_numbers if not number % num]
 
 print(result)
