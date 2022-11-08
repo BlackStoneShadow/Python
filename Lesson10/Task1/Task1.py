6f84f8c2a16211dd241f11ced90b7e8e3314750e
Foton <ekx79@yandex.ru>
2022-11-08 20:36:10 +0300

    Add graphic menu telegram bot calc

--- Lesson10/Task1/Task1.py
+++ Lesson10/Task1/Task1.py
@@ -1,6 +1,6 @@
 from controller import Controller
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'input command /menu')
@@ -18,6 +18,28 @@ async def plog(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def clog(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(cnt.menu_clog())
 
+async def start(bot, update):
+  await bot.message.reply_text(main_menu_message(),
+                         reply_markup=main_menu_keyboard())
+
+def main_menu(bot, update):
+  bot.callback_query.message.edit_text(main_menu_message(),
+                          reply_markup=main_menu_keyboard())
+
+def main_menu_message():
+  return 'Choose the option in main menu:'
+
+def main_menu_keyboard():
+  keyboard = [[InlineKeyboardButton('Print Log', callback_data='pl')],
+              [InlineKeyboardButton('Clear Log', callback_data='cl')]]
+  return InlineKeyboardMarkup(keyboard)
+
+async def menu_log_print(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:    
+    await update.callback_query.edit_message_text(cnt.menu_plog())
+
+async def menu_log_clear(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:    
+    await update.callback_query.edit_message_text(cnt.menu_clog())
+
 cnt = Controller()
 #print(cnt.menu_eval('1.1+20'))
 #print(cnt.menu_eval('1+1i+2+2i'))
@@ -29,5 +51,10 @@ app.add_handler(CommandHandler("menu", menu))
 app.add_handler(CommandHandler("eval", eval))
 app.add_handler(CommandHandler("plog", plog))
 app.add_handler(CommandHandler("clog", clog))
+
+app.add_handler(CommandHandler('start', start))
+app.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
+app.add_handler(CallbackQueryHandler(menu_log_print, pattern='pl'))
+app.add_handler(CallbackQueryHandler(menu_log_clear, pattern='сl'))
 
 app.run_polling()
