eff6b22d3a718c673e3a901bb85bddac2b0052c9
Tolik <ProtorskyAV@yandex.ru>
2023-02-19 22:32:55 +0300

    analytics

--- Analytics/Lesson1/Task1/Task1.py
+++ Analytics/Lesson1/Task1/Task1.py
@@ -2,9 +2,11 @@
 def filter(dic, key):
   return {k: v for k, v in dic.items() if k in key}
   
-filter({
+dic = filter({
 	"name": "John",
 	"age": 25,
 	"salary": 5000,
 	"city": "Moscow"
 }, ["name", "age"])
+
+print(dic)
