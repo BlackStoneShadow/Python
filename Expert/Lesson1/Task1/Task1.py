f6b67771ae4cf012545241955eeb831e1e37a579
Tolik <ProtorskyAV@yandex.ru>
2023-08-12 22:36:40 +0300

    Home Work Lesson1 Task1 Release

---
+++ Expert/Lesson1/Task1/Task1.py
@@ -0,0 +1,18 @@
+#Нарисовать в консоли ёлку спросив у пользователя количество рядов.def try_castinput(message, type):
+    result = str()
+   
+    try:
+        result = type(input(message))
+    except:
+        result = try_castinput(message, type)
+
+    return result
+
+n = try_castinput("Enter rows count:", int)
+
+for i in range(1, n + 1):
+    for j in range(1, n*2 + 1):
+        if n - i < j < n + i :
+            print("*", end="" if j < n*2 else "\n")
+        else:
+            print(" ", end="" if j < n*2 else "\n")
