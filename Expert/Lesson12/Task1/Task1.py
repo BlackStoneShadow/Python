2bcba1928c4f5a0a3d70a47c95e0b4d246baea32
Tolik <ProtorskyAV@yandex.ru>
2023-11-04 17:22:34 +0300

    Release Home Work Python Expert Lesson12

--- Expert/Lesson12/Task1/Task1.py
+++ Expert/Lesson12/Task1/Task1.py
@@ -9,6 +9,9 @@
 #предмета и по оценкам всех предметов вместе взятых. 
 import csv
 
+__GRADES__ = "grades"
+__SCORES__ = "test_scores"
+
 class Name:
     def __set_name__(self, owner, name):
         self.param_name = '_' + name
@@ -20,11 +23,11 @@ class Name:
         self.validate(value)
         setattr(instance, self.param_name, value)
 
-    def validate(self, value):
+    def validate(self, value):        
         if not isinstance(value, str):
-            raise AttributeError(f'Свойство "{self.param_name}" должно быть строкой')
-        elif not (str(value).isalpha or str(value).istitle):
-            raise AttributeError("ФИО должно состоять только из букв и начинаться с заглавной буквы")
+            raise ValueError(f'Свойство "{self.param_name}" должно быть строкой')
+        elif not (str(value).replace(" ", "").isalpha() and str(value).istitle()):
+            raise ValueError("ФИО должно состоять только из букв и начинаться с заглавной буквы")
 
 class Range:
     def __init__(self, min_value: int = None, max_value: int = None):
@@ -43,11 +46,11 @@ class Range:
 
     def validate(self, value):
         if not isinstance(value, int):
-            raise AttributeError(f'Свойство "{self.param_name}" должно быть целым числом')
+            raise ValueError(f'свойство "{self.param_name}" должно быть целым числом')
         elif self.min_value is not None and value < self.min_value:
-            raise AttributeError("значение {value} должно быть больше или равно {self.min_value}")
+            raise ValueError(f"значение {value} должно быть больше или равно {self.min_value}")
         elif self.max_value is not None and value > self.max_value:
-            raise AttributeError("значение {value} должно не привышвть {self.max_value}")
+            raise ValueError(f"{'Оценка должна' if self.param_name == '__Student__grade' else 'Результат теста должен'} быть целым числом от {self.min_value} до {self.max_value}")
 
 class Student:
     name = Name()
@@ -64,7 +67,7 @@ class Student:
         if name in self.subjects:
             return self.subjects[name]
         else:
-            raise AttributeError(f"Предмет {name} не найден")
+            raise ValueError(f"Предмет {name} не найден")
 
     def __setattr__(self, name, value):
         super().__setattr__(name, value)
@@ -74,35 +77,36 @@ class Student:
             self.__grade = grade
             self.subjects[subject]['grades'].append(grade)
         else:
-            raise AttributeError(f"Предмет {subject} не найден")
+            raise ValueError(f"Предмет {subject} не найден")
 
     def  add_test_score(self, subject, test_score):
         if subject in self.subjects:
             self.__test_scope = test_score
             self.subjects[subject]['test_scores'].append(test_score)
         else:
-            raise AttributeError(f"Предмет {subject} не найден")
+            raise ValueError(f"Предмет {subject} не найден")
 
-    def get_average_grade(self):
-        return (avg(item) for item in self.subjects)
+    def get_average_grade(self):         
+        return sum(sum(value[__GRADES__]) for key, value in self.subjects.items() if len(value[__GRADES__]) > 0) /\
+               sum(len(value[__GRADES__]) for key, value in self.subjects.items() if len(value[__GRADES__]) > 0) 
 
     def get_average_test_score(self, subject):
         if subject in self.subjects:
-            return avg(self.subjects[subject])
+            return sum(self.subjects[subject][__SCORES__]) / len(self.subjects[subject][__SCORES__])
         else:
-            raise AttributeError(f"Предмет {subject} не найден")
+            raise ValueError(f"Предмет {subject} не найден")
     
     def load_subjects(self, subjects_file):
         result = dict()
 
         with open(subjects_file, "r", encoding="utf-8") as f:        
             csv_reader = csv.reader(f, delimiter=",")            
-            result = {item:{"grades": [], "test_scores": []} for item in [col for col in [row for row in csv_reader]][0]}
+            result = {item:{__GRADES__: [], __SCORES__: []} for item in [col for col in [row for row in csv_reader]][0]}
         
         return result
 
     def __str__(self):
-        return f'Студент: {self.name}\nПредметы: {", ".join(self.subjects.keys())}'
+        return f'Студент: {self.name}\nПредметы: {", ".join(key for key, value in self.subjects.items() if len(value[__GRADES__]) > 0)}'
 
 if __name__ == "__main__": 
     student = Student("Иван Иванов", "subjects.csv")
