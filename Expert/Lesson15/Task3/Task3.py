6d755ba315f67a92f1ee7642f8899f2ad9a38f17
Foton <ekx79@yandex.ru>
2023-11-23 19:33:19 +0300

    Release Home Work Python Expert Lesson15

--- Expert/Lesson15/Task3/Task3.py
+++ Expert/Lesson15/Task3/Task3.py
@@ -1,10 +1,13 @@
 import argparse
 from module import Users
 
-parser = argparse.ArgumentParser(description='ProjectUser argument parser');parser.add_argument('-l', metavar='login', type=str, nargs='?', help='press login name');parser.add_argument('-i', metavar='id', type=str, nargs='?', help='press login id', default='0');parser.add_argument('-a', metavar='name', type=str, nargs=3, help='add user(name,level,id)', default=None);parser.add_argument('-s', metavar='show', type=str, nargs='?', help='show users', default=None);args = parser.parse_args()   users = Users.ProjectUser() 
-users.login_2_sys(args.l, args.i)
+def main():
+    parser = argparse.ArgumentParser(description='ProjectUser argument parser');    parser.add_argument('-l', metavar='login', type=str, nargs='?', help='press login name');    parser.add_argument('-i', metavar='id', type=str, nargs='?', help='press login id', default='0');    parser.add_argument('-a', metavar='value', type=str, nargs=3, help='add user(name,level,id)', default=None);    parser.add_argument('-s', metavar='show', type=str, nargs='?', help='show users', default=None);    args = parser.parse_args()       users = Users.ProjectUser() 
+    users.login_2_sys(args.l, args.i)
 
-if args.a is not None:
-    users.add_user(*args.a)
-elif args.s is not None:
-    print(users)
+    if args.a is not None:
+        users.add_user(*args.a)
+    elif args.s is not None:
+        print(users)
+
+main()
