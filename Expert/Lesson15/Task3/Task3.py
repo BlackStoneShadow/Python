c5c253a7c390bef6e0ff02f1eaed22eccd10dadc
Foton <ekx79@yandex.ru>
2023-11-23 19:24:25 +0300

    Release Home Work Python Expert Lesson15

--- Expert/Lesson15/Task3/Task3.py
+++ Expert/Lesson15/Task3/Task3.py
@@ -2,7 +2,8 @@ import argparse
 from module import Users
 
 parser = argparse.ArgumentParser(description='ProjectUser argument parser')
-parser.add_argument('-l', metavar='login', type=str, nargs='?', help='press login name')parser.add_argument('-i', metavar='id', type=str, nargs='?', help='press login id', default='0')parser.add_argument('-a', metavar='name', type=str, nargs=3, help='add user(name,level,id)', default=None)parser.add_argument('-s', metavar='show', type=str, nargs='?', help='show users', default=None)args = parser.parse_args()   users = Users.ProjectUser() 
+
+parser.add_argument('-l', metavar='login', type=str, nargs='?', help='press login name')parser.add_argument('-i', metavar='id', type=str, nargs='?', help='press login id', default='0')parser.add_argument('-a', metavar='name', type=str, nargs=3, help='add user(name,level,id)', default=None)parser.add_argument('-s', metavar='show', type=str, nargs='?', help='show users', default=None)args = parser.parse_args()   users = Users.ProjectUser() 
 users.login_2_sys(args.l, args.i)
 
 if args.a is not None:
