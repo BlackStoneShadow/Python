151d2eb14be8b0343c4ab6f43e3d98b96ebc2bb8
Foton <ekx79@yandex.ru>
2024-03-15 11:11:42 +0300

    Release Markup Home Work Lesson2

--- MLearning/Markup/Lesson2/Task1/Task1.py
+++ MLearning/Markup/Lesson2/Task1/Task1.py
@@ -3,11 +3,16 @@ import json
 from pathlib import Path
 from module import controller
 
-data = []
-for item in controller.Toscrape():
-    os.system("cls")
-    print(f"page:{item.page}")
-    data.append(item.books)
-
-with open(Path().cwd() / "list.json", "w", encoding="utf-8") as f:                
-    json.dump(data, f, ensure_ascii=False)         
+with open(Path().cwd() / "list.json", "w") as f:                
+    json.dump([], f)         
+    for item in controller.Toscrape():
+        os.system("cls")
+        print(f"page:{item.page}")        
+       
+        f.seek(0, os.SEEK_END)
+        f.seek(f.tell()-1, os.SEEK_SET)
+        f.truncate()
+        if f.tell() > 1:
+            f.write(',')
+        json.dump(item.books, f)
+        f.write(']')
