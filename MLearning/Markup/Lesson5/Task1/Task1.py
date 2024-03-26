0d52f9aab4249803f67960f1ecc9b345fea93837
Foton <ekx79@yandex.ru>
2024-03-26 22:01:11 +0300

    Release Markup Home Work Lesson5

--- MLearning/Markup/Lesson5/Task1/Task1.py
+++ MLearning/Markup/Lesson5/Task1/Task1.py
@@ -1,1 +1,14 @@
-print("hello world!")
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.reactor import install_reactor
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+
+from bankiru.spiders.news import NewsSpider
+
+if __name__ == "__main__":
+    configure_logging()
+    install_reactor("twisted.internet.asyncioreactor.AsyncioSelectorReactor")
+
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(NewsSpider)
+    process.start()
---
+++ MLearning/Markup/Lesson5/Task1/bankiru/spiders/news.py
@@ -0,0 +1,42 @@
+import re
+import scrapy
+from scrapy.http import HtmlResponse
+from datetime import datetime
+
+class NewsSpider(scrapy.Spider):
+    name = 'news'
+    allowed_domains = ['ru.investing.com']
+    start_urls = ['https://ru.investing.com/news/economy']    
+
+    def parse(self, response:HtmlResponse):        
+        next = response.xpath('//span[@class="hidden sm:block" and text()="След."]//parent::a/@href').get()
+        if next:
+            yield response.follow(next, callback=self.parse)
+
+        items = response.xpath('//div/a[contains(@class, "text-secondary")]')
+        for item in items:
+            name = item.xpath(".//text()").get()
+            link = item.xpath(".//@href").get()
+
+            yield response.follow(url=link, callback=self.parse_article, meta={
+                    "article_name": name,
+                    })
+
+    def parse_article(self, response:HtmlResponse):
+        try:
+            text = response.xpath('//h1[@id="articleTitle"]//parent::div//span[contains(text(), "Опубликовано")]').get()
+            date = datetime.strptime(re.search("\d{2}.\d{2}.\d{4}", text)[0], "%d.%m.%Y")
+        except ValueError as e:
+            date = None
+        
+        try:
+            text = response.xpath('//div[@id="article"]//child::p/text()').getall()        
+            text = str().join(_ for _ in text).strip()
+        except ValueError as e:
+            text = None            
+        
+        yield {
+            "article" : response.request.meta["article_name"],
+            "date"    : date,
+            "text"    : text,
+        }
