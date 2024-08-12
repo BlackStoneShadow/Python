f0c27f7d4245b99b7f9252692b068ca151f32fb1
Foton <ekx79@yandex.ru>
2024-08-12 18:42:57 +0300

    Edit Diplom Project

--- Diplom/Python3X.bat
+++
@@ -1,2 +0,0 @@
-PATH=C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python3X_64\Scripts\;C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python3X_64\;%PATH%%
-"c:\Program Files\Microsoft VS Code\Code.exe"
